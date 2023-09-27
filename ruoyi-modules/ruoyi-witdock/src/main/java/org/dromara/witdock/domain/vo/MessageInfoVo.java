f8c25182fa04d869dce818f3dd7c64f0278c5ef5
A70822 <zhouyuhang@liugong.com>
2023-09-27 14:01:10 +0800

    messageInfoVo

---
+++ ruoyi-modules/ruoyi-witdock/src/main/java/org/dromara/witdock/domain/vo/MessageInfoVo.java
@@ -0,0 +1,71 @@
+package org.dromara.witdock.domain.vo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.dromara.witdock.domain.MessageInfo;
+import com.alibaba.excel.annotation.ExcelIgnoreUnannotated;
+import com.alibaba.excel.annotation.ExcelProperty;
+import org.dromara.common.excel.annotation.ExcelDictFormat;
+import org.dromara.common.excel.convert.ExcelDictConvert;
+import io.github.linpeilie.annotations.AutoMapper;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+
+
+/**
+ * 对话消息视图对象 message_info
+ *
+ * @author Lion Li
+ * @date 2023-09-27
+ */
+@Data
+@ExcelIgnoreUnannotated
+@AutoMapper(target = MessageInfo.class)
+public class MessageInfoVo implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    /**
+     *
+     */
+    @ExcelProperty(value = "")
+    private Long id;
+
+    /**
+     * 对话id
+     */
+    @ExcelProperty(value = "对话id")
+    private Long conversationId;
+
+    /**
+     * 提问内容
+     */
+    @ExcelProperty(value = "提问内容")
+    private String query;
+
+    /**
+     * 回答内容
+     */
+    @ExcelProperty(value = "回答内容")
+    private String answer;
+
+    /**
+     * 回答时间
+     */
+    @ExcelProperty(value = "回答时间")
+    private Date reDatetime;
+
+    /**
+     * 花费token
+     */
+    @ExcelProperty(value = "花费token")
+    private BigDecimal msgToken;
+
+
+}
